837319ba3c2ae54364a7228372d9bedb255bf89e
Paulo André <pauloandre0011@gmail.com>
2020-09-04 15:24:32 -0300

    Gerando token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -28,7 +30,15 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
-        return { user };
+        const token = sign({}, 'b1853d231df03c02a782bb4f7b84664f', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
+        return {
+            user,
+            token,
+        };
     }
 }
 
