fe4af4805d59be975e5d7347f94b8734c8d733d0
Paulo André <pauloandre0011@gmail.com>
2020-09-16 16:01:38 -0300

    Adicionando rateLimiter

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -6,6 +6,7 @@ import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import AppError from '@shared/errors/AppError';
+import rateLimiter from './middlewares/rateLimiter';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
@@ -13,6 +14,7 @@ import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(express.json());
 app.use(routes);
 app.use(errors());
